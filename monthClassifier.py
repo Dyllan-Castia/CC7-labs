ec822fbcb38040d6917d5ebfeb79b879a295364d
Dyllan Castia <castiadyllan@gmail.com>
2026-02-16 14:14:13 -0800

    Add month classifier for seasons based on input

---
+++ monthClassifier.py
@@ -0,0 +1,20 @@
+input_month = input()
+input_day = int(input())
+""" Type your code here. """
+if ((input_month == "March" and input_day >= 20) or 
+     input_month in ["April", "May"] or 
+     (input_month == "June" and input_day <= 20)):
+    print("Spring")
+elif ((input_month == "June" and input_day >= 21) or 
+       input_month in ["July", "August"] or 
+       (input_month == "September" and input_day <= 21)):
+    print("Summer")
+elif ((input_month == "September" and input_day >= 22) or 
+       input_month in ["October", "November"] or 
+       (input_day == "December" and input_day <= 20)):
+    print("Autumn")
+elif ((input_month == "December" and input_day >= 21) or 
+       (input_month in ["January", "February"] and input_day < 31) or (input_month == "March" and input_day <= 21)):
+    print("Winter")
+else:
+    print("Invalid")
