65977b82c54c81dcb9c6a9b992b353b3fcbad957
Dyllan Castia <castiadyllan@gmail.com>
2026-02-16 16:47:39 -0800

    Fix date range conditions for season classification

--- monthClassifier.py
+++ monthClassifier.py
@@ -1,6 +1,7 @@
 input_month = input()
 input_day = int(input())
 """ Type your code here. """
+
 if ((input_month == "March" and input_day >= 20) or 
      input_month in ["April", "May"] or 
      (input_month == "June" and input_day <= 20)):
@@ -9,12 +10,12 @@ elif ((input_month == "June" and input_day >= 21) or
        input_month in ["July", "August"] or 
        (input_month == "September" and input_day <= 21)):
     print("Summer")
-elif ((input_month == "September" and input_day >= 22) or 
+elif ((input_month == "September" and 22 <= input_day <= 30) or 
        input_month in ["October", "November"] or 
        (input_day == "December" and input_day <= 20)):
     print("Autumn")
 elif ((input_month == "December" and input_day >= 21) or 
-       (input_month in ["January", "February"] and input_day < 31) or (input_month == "March" and input_day <= 21)):
+       (input_month in ["January", "February"] and (0 < input_day <= 31)) or (input_month == "March" and input_day <= 21)):
     print("Winter")
 else:
     print("Invalid")
