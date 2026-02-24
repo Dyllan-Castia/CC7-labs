70c6c24d3908a821521b0f6e23901e192a3bad56
Dyllan Castia <castiadyllan@gmail.com>
2026-02-24 12:29:22 -0800

    Add input/output examples to nested_row_column.py
    Added example input and output as comments.

--- nested_row_column.py
+++ nested_row_column.py
@@ -6,3 +6,11 @@ for row in range(num_rows):
     for col in range(1, num_columns + 1):
         print(f"{letter}{col}", end=" ")
     print()  
+#input:
+    #3
+    #5
+    
+#Output:
+#A1 A2 A3 A4 A5 
+#B1 B2 B3 B4 B5 
+#C1 C2 C3 C4 C5 
