5ca57251a280121ab9a81238751da0a3d0aa01b7
Dyllan Castia <castiadyllan@gmail.com>
2026-05-11 13:33:03 -0700

    Implement average exam scores calculation with NumPy

---
+++ data_science/basic_numpy_arrays/avg_exam_scores.py
@@ -0,0 +1,16 @@
+# TODO: Import NumPy
+import numpy as np
+
+# TODO: Read two sets of exam scores of five students from user input
+#       and store the scores into two NumPy arrays
+exam1 = np.array(list(map(int, input().split())))
+exam2 = np.array(list(map(int, input().split())))
+
+# TODO: Compute the average scores for each of the five students
+average_scores = (exam1 + exam2) / 2
+# TODO: Output "Average scores: " followed by the NumPy array of the average scores
+print("Average scores:", average_scores)
+# TODO: Count the number of average scores that are >= 80
+count = np.sum(average_scores >= 80)
+# TODO: Output "Number of students who received 80 and above: " followed by the count
+print("Number of students who received 80 and above:", count)
